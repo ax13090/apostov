ebcb68c6ed8266541bf487e135875c2dbda6eb29
Axel de Sablet <contact@axel.desablet.fr>
2016-02-28 15:49:18 +0100

    Add a comment about how PokerHandRanking implements Comparable

--- src/main/java/strength/PokerHandRanking.java
+++ src/main/java/strength/PokerHandRanking.java
@@ -1,7 +1,12 @@
 package strength;
 
-/**
- * 
+/*
+ * Note: this class implements Comparable in a peculiar way. One is supposed
+ * to call the compareTo() method only on two instances of the same subclass.
+ * For instance, if o1 and o2 are instances of PairRanking, the
+ * comparison o1.compareTo(o2) will work.
+ * But if o1 is an instance of PairRanking and o2 is an instance
+ * of StraightRanking, then o1.compareTo(o2) will throw a ClassCastException. 
  */
 public abstract class PokerHandRanking implements Comparable<PokerHandRanking> {
 
