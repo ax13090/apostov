cffca7587fb97e0419f61cffb7bdc9d109d29619
Axel de Sablet <contact@axel.desablet.fr>
2016-02-28 16:36:49 +0100

    Add a JUnit test for FullHouseRanking

--- src/test/java/strength/PokerHandRankingTest.java
+++ src/test/java/strength/PokerHandRankingTest.java
@@ -2,10 +2,14 @@ package strength;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Ordering;
 
+import apostov.Card;
 import apostov.Suit;
 import apostov.Value;
 
@@ -22,5 +26,27 @@ public class PokerHandRankingTest {
 		final StraightFlushRanking worstRanking = ordering.min(sf1, sf2, sf3);
 		assertEquals(Value.TEN, bestRanking.highestCardValue);
 		assertEquals(Value.EIGHT, worstRanking.highestCardValue);
+	}
+	
+	@Test
+	public void testFullHouseRanking() {
+		final FullHouseRanking fh1 = new FullHouseRanking(
+				new Card(Value.SIX, Suit.CLUBS),
+				new Card(Value.SIX, Suit.HEARTS),
+				new Card(Value.SIX, Suit.SPADES),
+				new Card(Value.TWO, Suit.HEARTS),
+				new Card(Value.TWO, Suit.DIAMONDS));
+		
+		final FullHouseRanking fh2 = new FullHouseRanking(
+				new Card(Value.TWO, Suit.CLUBS),
+				new Card(Value.TWO, Suit.HEARTS),
+				new Card(Value.TWO, Suit.SPADES),
+				new Card(Value.ACE, Suit.HEARTS),
+				new Card(Value.ACE, Suit.DIAMONDS));
+		
+		final List<FullHouseRanking> sortedCopy = FullHouseRanking.ordering().sortedCopy(ImmutableList.of(fh1, fh2));
+		assertSame(fh1, sortedCopy.get(1));
+		assertSame(fh2, sortedCopy.get(0));
+		
 	}
 }
