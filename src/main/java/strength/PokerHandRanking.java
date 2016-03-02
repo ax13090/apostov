23445416a7c39a59704fe5222b71bfa146ce9886
Axel de Sablet <contact@axel.desablet.fr>
2016-03-02 21:47:29 +0100

    Add some comments and warnings about the peculiar PokerHandRanking#compareTo()

--- src/main/java/strength/PokerHandRanking.java
+++ src/main/java/strength/PokerHandRanking.java
@@ -6,7 +6,10 @@ package strength;
  * For instance, if o1 and o2 are instances of PairRanking, the
  * comparison o1.compareTo(o2) will work.
  * But if o1 is an instance of PairRanking and o2 is an instance
- * of StraightRanking, then o1.compareTo(o2) will throw a ClassCastException. 
+ * of StraightRanking, then o1.compareTo(o2) will throw a ClassCastException.
+ * 
+ * Also, while this class defines a natural ordering for itself, this ordering
+ * is not consistent with equals().
  */
 public abstract class PokerHandRanking implements Comparable<PokerHandRanking> {
 
@@ -15,5 +18,16 @@ public abstract class PokerHandRanking implements Comparable<PokerHandRanking> {
 	public PokerHandRanking(final PokerHandKind handKind) {
 		this.handKind = handKind;
 	}
+	
+	/**
+	 * Only call this method to compare objects of the same type.
+	 * Otherwise a ClassCastException will be raised.
+	 * 
+	 * To compare two Poker-Hand-Rankings of different classes, use
+	 * a {@link PokerHandComparator}.
+	 */
+	@Override
+	@Deprecated
+	public abstract int compareTo(PokerHandRanking h);
 	
 }
