36380908b7391a6baa5ccf16533bf830c7b5cf7c
Axel de Sablet <contact@axel.desablet.fr>
2016-03-02 19:38:06 +0100

    Refactor two Maps into a Guava ImmutableTable

--- src/main/java/apostov/ShowdownEvaluator.java
+++ src/main/java/apostov/ShowdownEvaluator.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Supplier;
@@ -21,7 +22,9 @@ import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Ordering;
 
 import strength.FlushRanking;
 import strength.FullHouseRanking;
@@ -40,23 +43,16 @@ public class ShowdownEvaluator {
 		throw new UnsupportedOperationException("Not implemented yet");
 	}
 	
-	// TODO idea : use Guava's Table and ImmutableTable classes instead of two ImmutableMap<X, EnumSet<Y>> types
 	public PokerHandRanking selectBestCombination(final ImmutableCollection<Card> cards) {
 		assert cards.size() >= 5;
 		
-		/* Build a Map where each map-key is a Suit, and each map-value is a sorted Set of card Values  */
-		final ImmutableMap<Suit, EnumSet<Value>> valuesBySuit = mapValuesBySuit(cards);
-		
-		/* Build a Map where each map-key is a card Value, and each map-value is a set of card Suits */
-		final ImmutableMap<Value, EnumSet<Suit>> suitsByValue = mapSuitsByValues(cards);
+		final ImmutableTable<Value, Suit, Card> table = buildDoubleEntryTable(cards);
 		
 		/* Search for Straight-Flushes
 		 * This works by assuming that there can not be two straight-flushes
 		 * in different suits, which is true for Texas Holdem. */
 		for (final Suit suit : Suit.values()) {
-			final EnumSet<Value> mutableValues = valuesBySuit.get(suit);
-			if (mutableValues == null)
-				continue;
+			final Set<Value> mutableValues = table.column(suit).keySet();
 			if (mutableValues.size() < 5)
 				continue;
 			
@@ -70,11 +66,9 @@ public class ShowdownEvaluator {
 		/* Search for quads */
 		for (int i = ACE.ordinal(); TWO.ordinal() <= i; --i) {
 			final Value value = Value.values()[i];
-			final EnumSet<Suit> suitsForCurrentValue = suitsByValue.get(value);
-			if (suitsForCurrentValue == null)
-				continue;
+			final Set<Suit> suitsForCurrentValue = table.row(value).keySet();
 			if (suitsForCurrentValue.size() == 4) {
-				final Card kicker = findKicker(suitsByValue, ImmutableSet.of(value));
+				final Card kicker = findKicker(table, ImmutableSet.of(value));
 				return new QuadRanking(value, kicker);
 			}
 		}
@@ -82,35 +76,32 @@ public class ShowdownEvaluator {
 		/* Search for full-houses */
 		for (int i = ACE.ordinal(); TWO.ordinal() <= i; --i) {
 			final Value possibleSetValue = Value.values()[i];
-			final EnumSet<Suit> suitsForPossibleSetValue = suitsByValue.get(possibleSetValue);
-			if (suitsForPossibleSetValue == null)
-				continue;
-			assert suitsForPossibleSetValue.size() < 4;
-			if (suitsForPossibleSetValue.size() < 3)
+			final ImmutableMap<Suit, Card> cardsForSetMappedBySuit = table.row(possibleSetValue);
+			assert cardsForSetMappedBySuit.size() < 4;
+			if (cardsForSetMappedBySuit.size() < 3)
 				continue;
 
-			assert suitsForPossibleSetValue.size() == 3;
+			assert cardsForSetMappedBySuit.size() == 3;
 			for (int j = ACE.ordinal(); TWO.ordinal() <= j; --j) {
 				final Value possiblePairValue = Value.values()[j];
 				if (possibleSetValue == possiblePairValue)
 					continue;
 				
-				final EnumSet<Suit> suitsForPossiblePairValue = suitsByValue.get(possiblePairValue);
-				if (suitsForPossiblePairValue == null)
-					continue;
-				assert suitsForPossiblePairValue.size() < 3;
+				final ImmutableMap<Suit, Card> cardsForPairMappedBySuit = table.row(possiblePairValue);
+				final Set<Suit> suitsForPossiblePairValue = cardsForPairMappedBySuit.keySet();
+				assert cardsForPairMappedBySuit.size() < 3;
 				
 				if (suitsForPossiblePairValue.size() == 2) {
-					final Iterator<Suit> setSuits = suitsForPossibleSetValue.iterator();
-					final Iterator<Suit> pairSuits = suitsForPossiblePairValue.iterator();
+					final Iterator<Card> setCards = cardsForSetMappedBySuit.values().iterator();
+					final Iterator<Card> pairCards = cardsForPairMappedBySuit.values().iterator();
 					final FullHouseRanking fullHouseRanking = new FullHouseRanking(
-							new Card(possibleSetValue, setSuits.next()),
-							new Card(possibleSetValue, setSuits.next()),
-							new Card(possibleSetValue, setSuits.next()),
-							new Card(possiblePairValue, pairSuits.next()),
-							new Card(possiblePairValue, pairSuits.next()));
-					assert !setSuits.hasNext();
-					assert !pairSuits.hasNext();
+							setCards.next(),
+							setCards.next(),
+							setCards.next(),
+							pairCards.next(),
+							pairCards.next());
+					assert !setCards.hasNext();
+					assert !pairCards.hasNext();
 					
 					return fullHouseRanking;
 				}
@@ -121,37 +112,31 @@ public class ShowdownEvaluator {
 		 * This works by assuming that there can not be two flushes
 		 * in different suits, which is true for Texas Holdem. */
 		for (final Suit suit : Suit.values()) {
-			final EnumSet<Value> mutableValues = valuesBySuit.get(suit);
-			if (mutableValues == null)
-				continue;
-			if (mutableValues.size() < 5)
+			final ImmutableMap<Value, Card> cardsByValue = table.column(suit);
+			final Set<Value> valuesForThisSuit = cardsByValue.keySet();
+			if (valuesForThisSuit.size() < 5)
 				continue;
 			
-			final ImmutableList<Value> flushValues = copyOf(
+			final ImmutableList<Value> descendingFlushValues = copyOf(
 					copyOf(Value.values())
 					.reverse()
 					.stream()
-					.filter(v -> mutableValues.contains(v))
+					.filter(v -> valuesForThisSuit.contains(v))
 					.limit(5)
-					.iterator()
-			);
-			assert flushValues.size() == 5;
+					.iterator());
+			assert descendingFlushValues.size() == 5;
 			
 			return new FlushRanking(
 					suit, 
-					flushValues.get(0),
-					flushValues.get(1),
-					flushValues.get(2),
-					flushValues.get(3),
-					flushValues.get(4));
+					descendingFlushValues.get(0),
+					descendingFlushValues.get(1),
+					descendingFlushValues.get(2),
+					descendingFlushValues.get(3),
+					descendingFlushValues.get(4));
 		}
 		
 		/* Search for Straights */
-		final EnumSet<Value> allValues = cards
-			.stream()
-			.map(c -> c.value)
-			.collect(Collectors.toCollection(() -> EnumSet.noneOf(Value.class)));
-		final Optional<Value> optionalStraightStrength = searchFiveConsecutiveValues(allValues);
+		final Optional<Value> optionalStraightStrength = searchFiveConsecutiveValues(table.rowKeySet());
 		if (optionalStraightStrength.isPresent()) {
 			final Value straightTopValue = optionalStraightStrength.get();
 			final Value secondHighestCardValue = Value.values()[straightTopValue.ordinal() - 1];
@@ -163,26 +148,11 @@ public class ShowdownEvaluator {
 			else
 				bottomCardValue = Value.values()[straightTopValue.ordinal() - 4];
 			
-			final Card highestCard = new Card(
-					straightTopValue,
-					Iterables.get(suitsByValue.get(straightTopValue), 0));
-			
-			final Card secondHighestCard = new Card(
-					secondHighestCardValue,
-					Iterables.get(suitsByValue.get(secondHighestCardValue), 0)
-			);
-			final Card middleCard = new Card(
-					middleCardValue,
-					Iterables.get(suitsByValue.get(middleCardValue), 0)
-			);
-			final Card fourthCard = new Card(
-					fourthCardValue,
-					Iterables.get(suitsByValue.get(fourthCardValue), 0)
-			);
-			final Card bottomCard = new Card(
-					bottomCardValue,
-					Iterables.get(suitsByValue.get(bottomCardValue), 0)
-			);
+			final Card highestCard = Iterables.get(table.row(straightTopValue).values(), 0);
+			final Card secondHighestCard = Iterables.get(table.row(secondHighestCardValue).values(), 0);
+			final Card middleCard = Iterables.get(table.row(middleCardValue).values(), 0);
+			final Card fourthCard = Iterables.get(table.row(fourthCardValue).values(), 0);
+			final Card bottomCard = Iterables.get(table.row(bottomCardValue).values(), 0);
 			
 			return StraightRanking.create(
 					highestCard,
@@ -195,16 +165,14 @@ public class ShowdownEvaluator {
 		/* Search for Sets */
 		for (int i = ACE.ordinal(); TWO.ordinal() <= i; --i) {
 			final Value possibleSetValue = Value.values()[i];
-			final EnumSet<Suit> suitsForPossibleSetValue = suitsByValue.get(possibleSetValue);
-			if (suitsForPossibleSetValue == null)
-				continue;
+			final Set<Suit> suitsForPossibleSetValue = table.row(possibleSetValue).keySet();
 			assert suitsForPossibleSetValue.size() < 4;
 			if (suitsForPossibleSetValue.size() < 3)
 				continue;
 			assert suitsForPossibleSetValue.size() == 3;
 			
-			final Card firstKicker = findKicker(suitsByValue, ImmutableSet.of(possibleSetValue));
-			final Card secondKicker = findKicker(suitsByValue, ImmutableSet.of(possibleSetValue, firstKicker.value));
+			final Card firstKicker = findKicker(table, ImmutableSet.of(possibleSetValue));
+			final Card secondKicker = findKicker(table, ImmutableSet.of(possibleSetValue, firstKicker.value));
 			final Suit firstSuit, secondSuit, thirdSuit;
 			{
 				final Iterator<Suit> setSuits = suitsForPossibleSetValue.iterator();
@@ -220,40 +188,36 @@ public class ShowdownEvaluator {
 		/* Search for Two-Pairs and Pairs */
 		for (int i = ACE.ordinal(); TWO.ordinal() <= i; --i) {
 			final Value possibleHighPairValue = Value.values()[i];
-			final EnumSet<Suit> suitsForPossibleHighPairValue = suitsByValue.get(possibleHighPairValue);
-			if (suitsForPossibleHighPairValue == null)
+			final ImmutableMap<Suit, Card> highPairCardsBySuit = table.row(possibleHighPairValue);
+			if (highPairCardsBySuit.size() < 2)
 				continue;
-			if (suitsForPossibleHighPairValue.size() < 2)
-				continue;
-			assert suitsForPossibleHighPairValue.size() == 2;
+			assert highPairCardsBySuit.size() == 2;
 
 			final Card firstCardOfHighestPair, secondCardOfHighestPair;
 			{
-				final Iterator<Suit> highPairSuits = suitsForPossibleHighPairValue.iterator();
-				firstCardOfHighestPair = new Card(possibleHighPairValue, highPairSuits.next());
-				secondCardOfHighestPair = new Card(possibleHighPairValue, highPairSuits.next());
-				assert !highPairSuits.hasNext();
+				final Iterator<Card> highPairCards = highPairCardsBySuit.values().iterator();
+				firstCardOfHighestPair = highPairCards.next();
+				secondCardOfHighestPair = highPairCards.next();
+				assert !highPairCards.hasNext();
 			}
 
 			for (int j = i - 1; TWO.ordinal() <= j; --j) {
 				final Value possibleLowPairValue = Value.values()[j];
-				final EnumSet<Suit> suitsForPossibleLowPairValue = suitsByValue.get(possibleLowPairValue);
+				final ImmutableMap<Suit, Card> lowPairCardsBySuit = table.row(possibleLowPairValue);
 
-				if (suitsForPossibleLowPairValue == null)
+				if (lowPairCardsBySuit.size() < 2)
 					continue;
-				if (suitsForPossibleLowPairValue.size() < 2)
-					continue;
-				assert suitsForPossibleLowPairValue.size() == 2;
+				assert lowPairCardsBySuit.size() == 2;
 				
-				final Card kicker = findKicker(suitsByValue, ImmutableSet.of(possibleHighPairValue, possibleLowPairValue));
+				final Card kicker = findKicker(table, ImmutableSet.of(possibleHighPairValue, possibleLowPairValue));
 				
 				
 				final Card firstCardOfLowestPair, secondCardOfLowestPair;
 				{
-					final Iterator<Suit> lowPairSuits = suitsForPossibleLowPairValue.iterator();
-					firstCardOfLowestPair = new Card(possibleLowPairValue, lowPairSuits.next());
-					secondCardOfLowestPair = new Card(possibleLowPairValue, lowPairSuits.next());
-					assert !lowPairSuits.hasNext();
+					final Iterator<Card> lowPairCards = lowPairCardsBySuit.values().iterator();
+					firstCardOfLowestPair = lowPairCards.next();
+					secondCardOfLowestPair = lowPairCards.next();
+					assert !lowPairCards.hasNext();
 				}
 				
 				return new TwoPairsRanking(
@@ -265,15 +229,15 @@ public class ShowdownEvaluator {
 			}
 
 			final Card firstKicker = findKicker(
-					suitsByValue, 
+					table,
 					ImmutableSet.of(possibleHighPairValue));
 			
 			final Card secondKicker = findKicker(
-					suitsByValue,
+					table,
 					ImmutableSet.of(possibleHighPairValue, firstKicker.value));
 			
 			final Card thirdKicker = findKicker(
-					suitsByValue,
+					table,
 					ImmutableSet.of(possibleHighPairValue, firstKicker.value, secondKicker.value));
 
 			return new PairRanking(
@@ -289,14 +253,12 @@ public class ShowdownEvaluator {
 		final List<Card> bestCards = new ArrayList<>(5);
 		for (int i = ACE.ordinal(); TWO.ordinal() <= i; --i) {
 			final Value value = Value.values()[i];
-			final Set<Suit> suits = suitsByValue.get(value);
-			if (suits == null)
-				continue;
-			if (suits.isEmpty())
+			final ImmutableMap<Suit, Card> cardsBySuit = table.row(value);
+			if (cardsBySuit.isEmpty())
 				continue;
 			
-			final Suit suit = Iterables.get(suits, 0);
-			bestCards.add(new Card(value, suit));
+			final Card card = Iterables.get(cardsBySuit.values(), 0);
+			bestCards.add(card);
 			
 			if (bestCards.size() == 5)
 				break;
@@ -310,8 +272,16 @@ public class ShowdownEvaluator {
 				bestCards.get(4));
 	}
 
+	private ImmutableTable<Value, Suit, Card> buildDoubleEntryTable(final ImmutableCollection<Card> cards) {
+		final ImmutableTable.Builder<Value, Suit, Card> builder = ImmutableTable.builder();
+		for (final Card card : cards) {
+			builder.put(card.value, card.suit, card);
+		}
+		return builder.build();
+	}
+
 	private Card findKicker(
-			final ImmutableMap<Value, EnumSet<Suit>> suitsByValue,
+			final ImmutableTable<Value, Suit, Card> table,
 			final ImmutableSet<Value> excludedValues)
 	{
 		for (int i = ACE.ordinal(); TWO.ordinal() <= i; --i) {
@@ -319,30 +289,13 @@ public class ShowdownEvaluator {
 			if (excludedValues.contains(possibleKickerValue))
 				continue;
 			
-			final EnumSet<Suit> suitsForThisPossibleKicker = suitsByValue.get(possibleKickerValue);
-			if (suitsForThisPossibleKicker != null && suitsForThisPossibleKicker.size() > 0)
-				return new Card(possibleKickerValue, Iterables.get(suitsForThisPossibleKicker, 0));
+			final ImmutableMap<Suit, Card> row = table.row(possibleKickerValue);
+			final Set<Suit> suitsForThisPossibleKicker = row.keySet();
+			if (suitsForThisPossibleKicker.size() > 0)
+				return Iterables.get(row.entrySet(), 0).getValue();
 		}
 		
 		throw new RuntimeException("Failed to find a kicker");
-	}
-
-	private ImmutableMap<Suit, EnumSet<Value>> mapValuesBySuit(final ImmutableCollection<Card> cards) {
-		final Supplier<EnumSet<Value>> enumSetSupplier = () -> EnumSet.noneOf(Value.class);
-		final ImmutableMap<Suit, EnumSet<Value>> valuesBySuit = copyOf(cards.stream().collect(groupingBy(
-				c -> c.suit,
-				mapping(c -> c.value, toCollection(enumSetSupplier))
-		)));
-		return valuesBySuit;
-	}
-
-	private ImmutableMap<Value, EnumSet<Suit>> mapSuitsByValues(final ImmutableCollection<Card> cards) {
-		final Supplier<EnumSet<Suit>> enumSetSupplier = () -> EnumSet.noneOf(Suit.class);
-		final ImmutableMap<Value, EnumSet<Suit>> valuesBySuit = copyOf(cards.stream().collect(groupingBy(
-				c -> c.value,
-				mapping(c -> c.suit, toCollection(enumSetSupplier))
-		)));
-		return valuesBySuit;
 	}
 
 	private Optional<Value> searchFiveConsecutiveValues(final Set<Value> values) {
