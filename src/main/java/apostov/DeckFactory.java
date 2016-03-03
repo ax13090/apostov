d60962f300cb5ed225b7ef4fd88b52fae5d9bf95
Axel de Sablet <contact@axel.desablet.fr>
2016-03-03 19:06:49 +0100

    Teach the Deck Factory to skip some cards while creating a deck

--- src/main/java/apostov/DeckFactory.java
+++ src/main/java/apostov/DeckFactory.java
@@ -3,18 +3,35 @@ package apostov;
 import java.util.EnumSet;
 import java.util.Set;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableList;
 
 public class DeckFactory {
 
-	public ImmutableSet<Card> newDeck() {
+	public ImmutableList<Card> newFullDeck() {
 		final Set<Suit> suits = EnumSet.allOf(Suit.class);
 		final Set<Value> values = EnumSet.allOf(Value.class);
 		
-		final ImmutableSet.Builder<Card> builder = ImmutableSet.builder();
+		final ImmutableList.Builder<Card> builder = ImmutableList.builder();
 		for (final Suit suit : suits)
 			for (final Value value : values)
 				builder.add(new Card(value, suit));
+		
+		return builder.build();
+	}
+	
+	public ImmutableList<Card> newDeck(final Set<Card> excludedCards) {
+		final Set<Suit> suits = EnumSet.allOf(Suit.class);
+		final Set<Value> values = EnumSet.allOf(Value.class);
+		
+		final ImmutableList.Builder<Card> builder = ImmutableList.builder();
+		for (final Suit suit : suits)
+			for (final Value value : values) {
+				final Card newCard = new Card(value, suit);
+				if (excludedCards.contains(newCard))
+					continue;
+				else
+					builder.add(newCard);
+			}
 		
 		return builder.build();
 	}
