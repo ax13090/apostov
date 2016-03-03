facb319f17afd97fc963ce4fdb0edda4ac5f6dba
Axel de Sablet <contact@axel.desablet.fr>
2016-03-03 21:05:11 +0100

    Use all the available CPU cores.

--- src/main/java/apostov/HandResultEnumerator.java
+++ src/main/java/apostov/HandResultEnumerator.java
@@ -6,6 +6,10 @@ import static java.util.stream.Collectors.toSet;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -30,22 +34,29 @@ public class HandResultEnumerator {
 		final ShowdownEvaluator evaluator = new ShowdownEvaluator();
 		final int binomialCoefficient = (int) CombinatoricsUtils.binomialCoefficient(deck.size(), 5);
 		
-		final Map<HolecardHand, Fraction> overallSuccessChanceByHand = StreamSupport.stream(new Combinations(deck.size(), 5).spliterator(), false)
-			.map((a) -> stream(a).mapToObj(i -> deck.get(i)).collect(toList()))
-			.map(l -> {
-				assert l.size() == 5;
-				return new Board(l.get(0), l.get(1), l.get(2), l.get(3), l.get(4));
-			})
-			.map(b -> evaluator.evaluateShowdown(candidates, b))
-			.map(wc -> Maps.toMap(wc, w -> new Fraction(1, binomialCoefficient * wc.size())))
-			.map(Map::entrySet)
-			.flatMap(Collection::stream)
-			.collect(Collectors.toMap(
+		final ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+		final Map<HolecardHand, Fraction> overallSuccessChanceByHand;
+		final Callable<ConcurrentMap<HolecardHand, Fraction>> successChanceComputationTask = 
+			() -> StreamSupport.stream(new Combinations(deck.size(), 5).spliterator(), true)
+				.map((a) -> stream(a).mapToObj(i -> deck.get(i)).collect(toList()))
+				.map(l -> {
+					assert l.size() == 5;
+					return new Board(l.get(0), l.get(1), l.get(2), l.get(3), l.get(4));
+				})
+				.map(b -> evaluator.evaluateShowdown(candidates, b))
+				.map(wc -> Maps.toMap(wc, w -> new Fraction(1, binomialCoefficient * wc.size())))
+				.map(Map::entrySet)
+				.flatMap(Collection::stream)
+				.collect(Collectors.toConcurrentMap(
 						Map.Entry::getKey,
 						Map.Entry::getValue,
-						(x, y) -> x.add(y)
-					)
-			);
+						(x, y) -> x.add(y)));
+		
+		try {
+			overallSuccessChanceByHand = pool.submit(successChanceComputationTask).get();
+		} catch (final InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
 		
 		return ImmutableMap.copyOf(overallSuccessChanceByHand);
 	}
