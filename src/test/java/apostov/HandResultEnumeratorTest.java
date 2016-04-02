f70262d8d5a7fea1035493c11ef309d23e73ff71
Axel de Sablet <contact@axel.desablet.fr>
2016-04-02 18:52:21 +0200

    Make a test a little more atomated, remove the need for human-eye verification

--- src/test/java/apostov/HandResultEnumeratorTest.java
+++ src/test/java/apostov/HandResultEnumeratorTest.java
@@ -10,6 +10,7 @@ import static apostov.Value.SEVEN;
 import static apostov.Value.TEN;
 import static apostov.Value.TWO;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
 
@@ -26,7 +27,9 @@ public class HandResultEnumeratorTest {
 		final HolecardHand aces = new HolecardHand(new Card(ACE, CLUBS), new Card(ACE, SPADES));
 		final HolecardHand trash = new HolecardHand(new Card(TWO, CLUBS), new Card(SEVEN, SPADES));
 		
-		displayPercentages(aces, trash);
+		final Map<HolecardHand, Fraction> percentages = computePercentages(aces, trash);
+		assertTrue(percentages.get(aces).doubleValue() > 0.8);
+		assertTrue(percentages.get(trash).doubleValue() < 0.2);
 	}
 
 	@Test
@@ -34,15 +37,18 @@ public class HandResultEnumeratorTest {
 		final HolecardHand aceTen = new HolecardHand(new Card(ACE, CLUBS), new Card(TEN, SPADES));
 		final HolecardHand queenTen = new HolecardHand(new Card(QUEEN, DIAMONDS), new Card(TEN, HEARTS));
 		
-		displayPercentages(aceTen, queenTen);
+		final Map<HolecardHand, Fraction> percentages = computePercentages(aceTen, queenTen);
+		assertTrue(percentages.get(aceTen).doubleValue() > 0.7);
+		assertTrue(percentages.get(queenTen).doubleValue() < 0.3);
 	}
 
-	private void displayPercentages(final HolecardHand h1, final HolecardHand h2) {
+	private Map<HolecardHand, Fraction> computePercentages(final HolecardHand h1, final HolecardHand h2) {
 		final Map<HolecardHand, Fraction> result = new HandResultEnumerator().enumerateBoardsAndMeasureWins(ImmutableList.of(h1, h2));
 		final Fraction sum = result.values().stream().reduce((x,y) -> x.add(y)).get();
 		assertEquals(1, sum.intValue());
 		
-		System.out.println(Maps.transformValues(result, f -> String.format("%.02f%%", 100*f.doubleValue())));
+		System.out.println(Maps.transformValues(result, f -> String.format("%.02f%%", 100 * f.doubleValue())));
+		return result;
 	}
 	
 }
