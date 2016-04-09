33e7885680d46facc4b5b716144c9b4e4865e935
Axel de Sablet <contact@axel.desablet.fr>
2016-04-09 18:34:03 +0200

    Use a safer version of a long-to-int downcasting

--- src/main/java/apostov/HandResultEnumerator.java
+++ src/main/java/apostov/HandResultEnumerator.java
@@ -2,6 +2,7 @@ package apostov;
 
 import static com.google.common.collect.Iterators.concat;
 import static com.google.common.collect.Maps.toMap;
+import static java.lang.Math.toIntExact;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
@@ -46,25 +47,27 @@ public class HandResultEnumerator {
 		
 		final ShowdownEvaluator evaluator = new ShowdownEvaluator();
 		final int numberOfUnknownCards = 5 - partialBoard.size();
-		final int numberOfTestedBoards = (int) CombinatoricsUtils.binomialCoefficient(deck.size(), numberOfUnknownCards);
+		final long numberOfTestedBoardsAsLong = CombinatoricsUtils.binomialCoefficient(deck.size(), numberOfUnknownCards);
 		
 		final ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
 		final Callable<? extends Map<HolecardHand, Fraction>> successChanceComputationTask = 
-			() -> StreamSupport.stream(new Combinations(deck.size(), numberOfUnknownCards).spliterator(), true)
-				.map(a -> stream(a).mapToObj(i -> deck.get(i)).collect(toList()))
-				.map(pickedCards -> {
-					assert pickedCards.size() == numberOfUnknownCards;
-					final Iterator<Card> fullBoard = concat(partialBoard.iterator(), pickedCards.iterator());
-					return Board.from(fullBoard);
-				})
-				.map(b -> evaluator.evaluateShowdown(candidates, b))
-				.map(winningCandidates -> toMap(winningCandidates, c -> new Fraction(1, numberOfTestedBoards * winningCandidates.size())))
-				.map(Map::entrySet)
-				.flatMap(Collection::stream)
-				.collect(Collectors.toConcurrentMap(
-						Map.Entry::getKey,
-						Map.Entry::getValue,
-						(x, y) -> x.add(y)));
+			() -> {
+				return StreamSupport.stream(new Combinations(deck.size(), numberOfUnknownCards).spliterator(), true)
+					.map(a -> stream(a).mapToObj(i -> deck.get(i)).collect(toList()))
+					.map(pickedCards -> {
+						assert pickedCards.size() == numberOfUnknownCards;
+						final Iterator<Card> fullBoard = concat(partialBoard.iterator(), pickedCards.iterator());
+						return Board.from(fullBoard);
+					})
+					.map(b -> evaluator.evaluateShowdown(candidates, b))
+					.map(winningCandidates -> toMap(winningCandidates, c -> new Fraction(1, toIntExact(numberOfTestedBoardsAsLong * winningCandidates.size()))))
+					.map(Map::entrySet)
+					.flatMap(Collection::stream)
+					.collect(Collectors.toConcurrentMap(
+							Map.Entry::getKey,
+							Map.Entry::getValue,
+							(x, y) -> x.add(y)));
+			};
 		
 		final Map<HolecardHand, Fraction> overallSuccessChanceByHand;
 		try {
